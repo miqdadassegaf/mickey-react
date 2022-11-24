95815b05a408a2d1b64f43cc9e9b3765d4cc12fa
mickey <mickey@centos-docker.com>
2022-11-24 13:57:14 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          With the lights out, its less dangerous
+	  Here we are now, entertain us
+	  I feel stupid and contagious
+	  Here we are now, entertain us
+	  A mulatto, an albino, a mosquito, my libido
+
+	  Yeah, hey
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
